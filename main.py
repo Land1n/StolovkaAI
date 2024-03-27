a6e98ca8199acd9214907cc81efc3ab0af7c32eb
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2024-03-28 00:50:18 +0700

    v1.0.2 update README.md

--- main.py
+++ main.py
@@ -2,21 +2,10 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+
 model = YOLO('yolov8n.pt')
 
-video = cv2.VideoCapture(r'C:\Users\User\Desktop\work\Stolovka_AI\AI\video\file1.mp4')
-
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+video = cv2.VideoCapture(r'C:\Users\User\Desktop\work\Stolovka_AI\AI\video.mp4')
 
 while True:
     _, img = video.read()
@@ -28,7 +17,7 @@ while True:
         boxes = r.boxes
         for box in boxes:
             cls = int(box.cls[0])
-            if classNames[cls] == 'person':
+            if cls == 0:
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 w, h = x2 - x1, y2 - y1
@@ -37,5 +26,5 @@ while True:
                 conf = math.ceil((box.conf[0] * 100)) / 100
                 cls = int(box.cls[0])  
     
-                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1,offset=3)
+                cvzone.putTextRect(img, f'person {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1,offset=3)
     cv2.imshow('Result',img)
