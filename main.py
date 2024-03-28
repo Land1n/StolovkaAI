eb3f1088fa55a03b4f43c96e6931ed0cb99879f4
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2024-03-28 13:10:29 +0700

    v1.0.3

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ model = YOLO('yolov8n.pt')
 
 video = cv2.VideoCapture(r'C:\Users\User\Desktop\work\Stolovka_AI\AI\video.mp4')
 
+mask = cv2.imread('mask.png')
+
 while True:
     _, img = video.read()
-    results = model(img, stream=True)
+    mask = cv2.resize(mask,(img.shape[1],img.shape[0]))
+    imgRegion = cv2.bitwise_and(img,mask)
+    results = model(imgRegion, stream=True)
+    cv2.rectangle(img,(300,400),(500,600),(0,0,255),5)
     k = cv2.waitKey(1)
     if k == ord("q"):
         break
@@ -24,7 +29,5 @@ while True:
 
                 cvzone.cornerRect(img, (x1, y1, w, h),l=9)
                 conf = math.ceil((box.conf[0] * 100)) / 100
-                cls = int(box.cls[0])  
-    
                 cvzone.putTextRect(img, f'person {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1,offset=3)
     cv2.imshow('Result',img)
